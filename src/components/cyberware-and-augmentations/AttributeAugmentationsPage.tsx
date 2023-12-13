b094bde62fb9164a83b5bd3ab5ecb7a543416910
QPGDEV <qpgdevelopment@gmail.com>
2023-12-12 23:21:23 -0500

    feat: added ul element to AttributeAugmentationsPage

--- src/components/cyberware-and-augmentations/AttributeAugmentationsPage.tsx
+++ src/components/cyberware-and-augmentations/AttributeAugmentationsPage.tsx
@@ -1,8 +1,19 @@
 const AttributeAugmentationsPage = () => {
-    return (
-        <div className="attribute-augmentations-page">
-        </div>
-    )
-}
+  return (
+    <div className="attribute-augmentations-page">
+      <p className="setting-the-scene-title"> attribute augmentations </p>
+      <p>
+        Attribute-specific Augmentations will cause a character to gain 1
+        Cyberware Density for each Attribute dot gained.
+      </p>
+      <ul style={{ listStyleType: "disc" }}>
+        <li>
+          Every dot gained through Augmentations in Physical Attributes also
+          grants +1 level of Armor.
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default AttributeAugmentationsPage
+export default AttributeAugmentationsPage;
